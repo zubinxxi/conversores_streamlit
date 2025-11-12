50bcb971183b88f44c8fad982a9a499957061ded
Zubin Juárez <zubinxxi@gmail.com>
2025-11-12 15:19:07 -0500

    Se agrgó comentario con el url del API de CurrencyLayer

--- cambio.py
+++ cambio.py
@@ -1,4 +1,8 @@
 import streamlit as st 
 
+#https://apilayer.net/api/live?access_key=b99fa0112782991a9bd2bd95ad2affee&currencies=EUR&source=USD&format=1
+
 def cambio():
     st.subheader("Conversor de Monedas")
+
+    
