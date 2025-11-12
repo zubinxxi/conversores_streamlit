7c5b84832c5df5ab757cf9e862d57ce8317225dc
Zubin Juárez <zubinxxi@gmail.com>
2025-11-12 08:13:51 -0500

    Se agregó la propiedad placeholder a los selectbox

--- conversor.py
+++ conversor.py
@@ -24,8 +24,8 @@ def conversor():
 
     col1, col2, col3 = st.columns(3)
     valor = col1.number_input("introduce el valor a convertir", min_value=0.0, value=1.0)
-    origen = col2.selectbox("Unidad de origen", options=unidades)
-    destino = col3.selectbox("Unidad de destino", options=unidades)
+    origen = col2.selectbox("Unidad de origen", options=unidades, placeholder="Select")
+    destino = col3.selectbox("Unidad de destino", options=unidades, placeholder="Select")
 
     if st.button("Convertir", type="primary"):
         resultado = conversor_unidades(valor, origen, destino)
